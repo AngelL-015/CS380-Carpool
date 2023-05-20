d361064f6a3cb0eec2c30e1c729801004076d815
Angel Lopez <LAngel@cwu.edu>
2023-05-19 19:18:59 -0700

    Added Review.java

---
+++ Review.java
@@ -0,0 +1,46 @@
+public class Review {
+	
+	// Variables
+	private int driverID;
+	private int passengerID;
+	private int rating;
+	private String comment;
+	
+	// Constructor
+	public Review() {	
+	}
+	
+	// Getters & Setters
+	public int getDriverID() {
+		return driverID;
+	}
+
+	public void setDriverID(int driverID) {
+		this.driverID = driverID;
+	}
+
+	public int getPassengerID() {
+		return passengerID;
+	}
+
+	public void setPassengerID(int passengerID) {
+		this.passengerID = passengerID;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
